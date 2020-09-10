62c02dd55b6cb0c5ccb2aff7f8d9137745d65fa8
ssr <stephan.strittmatter@sybit.de>
2020-09-10 11:55:26 +0200

    empty the app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app" class="container">
-    <div id="nav">
-    </div>
     <router-view/>
-    <bottom-navigation></bottom-navigation>
-    <notifications group="notification" class="mt-3" position="top center" />
   </div>
 </template>
 
 <script>
-import BottomNavigation from '@/components/navigation/BottomNavigation.vue'
+
 export default {
   name: 'App',
   components: {
-    BottomNavigation
+
   },
   metaInfo () {
     return {
@@ -30,13 +26,5 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-.container{
-  padding: 0; // Padding has to be Zeor for Vertical Impulse Scroll List, to show Full Element.
-  max-width: 100%;
-}
+
 </style>
