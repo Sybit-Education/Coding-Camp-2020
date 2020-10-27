3436a3568c79c1a62d4e26021e9732f7179213b1
robingundlach <69695642+robingundlach@users.noreply.github.com>
2020-10-27 15:53:03 +0100

    Fix Comments

--- src/components/cards/ImpulseCard.vue
+++ src/components/cards/ImpulseCard.vue
@@ -2,23 +2,22 @@
    <div class="card-body">
        <div class="card-content">
            <div class="card-text"><p>Fahrrad fahren ist gut für die Umwelt.</p></div>
-           <div class="card-text card-text-space"><p>Da man kein CO<sub>2</sub> außstöst. Das ist mehr Text um zu zeigen das hier dann alles auf die SChnauze fliegen wird. lül.</p></div>
+           <div class="card-text card-text-space"><p>Da man kein CO<sub>2</sub> außstöst. Das ist mehr Text um zu zeigen das hier dann alles auf die Schnauze fliegen wird. lül.</p></div>
        </div>
        <div class="card-background">
             <img  src="@/assets/cards/Card-blue.svg">
        </div>
    </div>
 </template>
-<script>
-export default {
-}
-</script>
 <style lang="scss" scoped>
 .card-background{
-width: 100%;}
+width: 100%;
+}
+
 .card-background img{
 width: 100%;
 }
+
 .card-body{
 margin-left: auto;
 margin-right: auto;
@@ -27,10 +26,12 @@ position:relative;
 padding: 0;
 height: 100%;
 }
+
 .card-content{
 position: absolute;
 height: 100%;
 }
+
 .card-text{
 height: 50%;
 text-align: center;
@@ -39,6 +40,7 @@ padding-bottom: 10px;
 padding-left: 15px;
 padding-right: 15px;
 }
+
 .card-text-space{
 padding-top: 30px
 }
