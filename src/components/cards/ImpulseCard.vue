54b3e6740cd1ae8837c3f8512521793d0637e59a
robingundlach <69695642+robingundlach@users.noreply.github.com>
2020-10-27 14:52:08 +0100

    Added Title

--- src/components/cards/ImpulseCard.vue
+++ src/components/cards/ImpulseCard.vue
@@ -1,9 +1,33 @@
 <template>
-   <div>
-        <img src="@/assets/cards/Card-blue.svg">
+   <div class="card-body">
+       <div class="card-content">
+           <h4>Hallo Welt</h4>
+       </div>
+       <div class="card-background">
+            <img  src="@/assets/cards/Card-blue.svg">
+       </div>
    </div>
 </template>
 <script>
 export default {
 }
 </script>
+<style lang="scss" scoped>
+.card-background{
+width: 100%;}
+.card-background img{
+width: 100%;
+}
+.card-body{
+margin-left: auto;
+margin-right: auto;
+width: 80%;
+position:relative;
+padding: 0;
+}
+.card-content{
+position: absolute;
+padding: 10%;
+text-align: center;
+}
+</style>
