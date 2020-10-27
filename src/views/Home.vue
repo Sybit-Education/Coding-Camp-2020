399d26bcb25b9087470cb1e581301736490dc6a3
Lasse17 <lasse@finetech.info>
2020-10-27 15:44:52 +0100

    Update-home.vue
    add change link to rounter-link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
- <b-link href="/signup">Sign-Up </b-link><br>
- <b-link href="/login">Login</b-link>
+ <rounter-link href="/signup">Sign-Up </rounter-link><br>
+ <rounter-link href="/login">Login</rounter-link>
     </div>
 </template>
