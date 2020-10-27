53f7e3593e3932e28e5540b5430a969747804575
robingundlach <69695642+robingundlach@users.noreply.github.com>
2020-10-27 15:37:45 +0100

    Added Impulse Card
    Rearange margin and paddings

--- src/components/cards/ImpulseCard.vue
+++ src/components/cards/ImpulseCard.vue
@@ -1,7 +1,8 @@
 <template>
    <div class="card-body">
        <div class="card-content">
-           <h4>Hallo Welt</h4>
+           <div class="card-text"><p>Fahrrad fahren ist gut für die Umwelt.</p></div>
+           <div class="card-text card-text-space"><p>Da man kein CO<sub>2</sub> außstöst. Das ist mehr Text um zu zeigen das hier dann alles auf die SChnauze fliegen wird. lül.</p></div>
        </div>
        <div class="card-background">
             <img  src="@/assets/cards/Card-blue.svg">
@@ -24,10 +25,21 @@ margin-right: auto;
 width: 80%;
 position:relative;
 padding: 0;
+height: 100%;
 }
 .card-content{
 position: absolute;
-padding: 10%;
+height: 100%;
+}
+.card-text{
+height: 50%;
 text-align: center;
+padding-top: 25px;
+padding-bottom: 10px;
+padding-left: 15px;
+padding-right: 15px;
+}
+.card-text-space{
+padding-top: 30px
 }
 </style>
