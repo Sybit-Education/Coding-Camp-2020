5468e36b65f33585e4c952639811a5e06058501d
Paul <frankypie202@gmail.com>
2020-10-28 11:59:05 +0100

    Corrected Cookie Banner Text

--- src/components/element/CookieBanner.vue
+++ src/components/element/CookieBanner.vue
@@ -2,7 +2,7 @@
   <footer>
     <cookie-law
     buttonText="Einverstanden!"
-    message="Um unsere Website zu optimieren und für Sie individueller zu gestalten, setzen wir Cookies für verschiedene Zwecke ein. Einige sind essentiell während andere optional aktiviert und deaktiviert werden können. Umfassende Informationen zu diesen Cookies finden Sie in unserer Datenschutzerklärung. Dort können Sie Ihre ganz individuellen Einstellungen jederzeit über den Button unten links anpassen."
+    message="Um unsere Website zu optimieren und für Sie individueller zu gestalten, setzen wir Cookies für verschiedene Zwecke ein. Umfassende Informationen zu diesen Cookies finden Sie in unserer Datenschutzerklärung."
     buttonLinkText="Datenschutzerklärung"
     buttonLink="/privacy"
     ></cookie-law>
