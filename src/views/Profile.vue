bc326b8d8e4e93248705c8d34b86a6f739e8e1e7
mirko-pleli <Mirko.Pleli@sybit.de>
2020-10-28 13:59:18 +0100

    Integrarion of user data.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,31 +6,53 @@
             </b-col>
         </b-row>
         <b-container>
-            <b-row>
-                <b-col>
-                    <p>Name</p>
-                </b-col>
-            </b-row>
-             <b-row>
-                <b-col class="d-flex justify-content-end ">
-                    <b-avatar src="https://placekitten.com/300/300" class="mt-n5 mr-3" size="4.5rem"></b-avatar>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col>
-                    <p>Email</p>
-                </b-col>
-            </b-row>
+          <b-row class="avatar-name align-self-center">
+            <b-col cols="3">
+              <avatar v-if="user" />
+            </b-col>
+            <b-col class="align-self-center">
+              <user-display-name :isEditable="true"/>
+              <small>{{user.email}}</small>
+            </b-col>
+          </b-row>
         </b-container>
             <b-row class="mt-3 border" >
               <b-col class="flex-row  d-flex justify-content-between align-items-center pt-2">
-                <p>Platz.</p><p>8</p><p>Punkte</p><p>9999</p>
+                <p>Platz</p><p>8</p><p>Punkte</p><p>9999</p>
                 </b-col>
             </b-row>
     </div>
 </template>
 <script>
-export default {}
+import { mapActions, mapGetters } from 'vuex'
+import { $auth } from '@/firebase-config'
+import Avatar from '@/components/_base/Avatar.vue'
+import UserDisplayName from '@/components/_base/UserDisplayName.vue'
+
+export default {
+  name: 'Profile',
+  components: {
+    UserDisplayName,
+    Avatar
+  },
+  async created () {
+    await this.fetchById(this.user.uid)
+  },
+  computed: {
+    user () {
+      return $auth.currentUser
+    },
+    ...mapGetters({
+      isAdmin: 'Userdata/isAdmin'
+    })
+  },
+  methods: {
+    ...mapActions({
+      signOut: 'Auth/signOut',
+      fetchById: 'Userdata/fetchById'
+    })
+  }
+}
 </script>
 <style lang="scss" scoped>
 .border{
