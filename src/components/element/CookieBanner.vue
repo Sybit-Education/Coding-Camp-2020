64006e4413b491309cff75be4e4e89596efdd7d0
Paul <frankypie202@gmail.com>
2020-10-28 11:16:39 +0100

    Added Cookie Banner

---
+++ src/components/element/CookieBanner.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <cookie-law
+    buttonText="Einverstanden!"
+    message="Um unsere Website zu optimieren und für Sie individueller zu gestalten, setzen wir Cookies für verschiedene Zwecke ein. Einige sind essentiell während andere optional aktiviert und deaktiviert werden können. Umfassende Informationen zu diesen Cookies finden Sie in unserer Datenschutzerklärung. Dort können Sie Ihre ganz individuellen Einstellungen jederzeit über den Button unten links anpassen."
+    buttonLinkText="Bite"
+    ></cookie-law>
+  </footer>
+</template>
+
+<script>
+import CookieLaw from 'vue-cookie-law'
+export default {
+  components: { CookieLaw }
+}
+
+</script>
