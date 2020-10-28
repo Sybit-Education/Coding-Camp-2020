f180aa69b912ab04c03ebf0edbc21a5426520130
mirko-pleli <Mirko.Pleli@sybit.de>
2020-10-28 09:34:41 +0100

    Resolve pr comments, add and register the Headline.vue as global component.

---
+++ src/components/_base/Headline.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1 class="heading">{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.heading {
+  margin-top: 30px;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,28 +1,23 @@
 <template>
-<div class="background">
-<!-- Bootstrap container für den Abstand und die mittige Platzierung !-->
+<div>
 <b-container center>
     <div>
-
         <b-row>
             <b-col>
-                <!-- Überschrift  !-->
-                <h1 class="u">ÜBER UNS</h1>
+                <div>
+                  <headline title="Über uns"></headline>
+                </div>
                 <br><br>
             </b-col>
         </b-row>
-
         <b-row>
             <b-col class="px-0">
                 <!-- Gruppenbild !-->
-                <b-img class="image" src="https://picsum.photos/1024/400/?image=41" rounded fluid-grow alt="Responsive image"></b-img>
+                <b-img src="https://picsum.photos/1024/400/?image=41" rounded fluid-grow alt="Responsive image"></b-img>
             </b-col>
         </b-row>
         <br>
-        <!-- Liste von Namen !-->
         <b-row class="d-flex justify-content-between names">
-
-            <!-- Erste Spalte !-->
             <b-col cols="12" md="4" class="d-flex justify-content-start flex-column align-items-start">
                 <p>Mario Sieg</p>
                 <p>Alexander Temirbulatow</p>
@@ -32,8 +27,6 @@
                 <p>Enrico Franke</p>
                 <p>Stephan Strittmatter</p>
             </b-col>
-
-            <!-- Zweite Spalte !-->
             <b-col cols="12" md="4" class="d-flex justify-content-start flex-column align-items-center">
                 <p>Manuel Metzger</p>
                 <p>Paul Huber</p>
@@ -44,8 +37,6 @@
                 <p>Elias Frank</p>
                 <p>Emely Langer</p>
             </b-col>
-
-            <!-- Dritte Spalte !-->
             <b-col cols="12" md="4" class="d-flex justify-content-start flex-column align-items-end">
                 <p>Lukas Moßbrugger</p>
                 <p>Lasse Schneider</p>
@@ -56,44 +47,19 @@
                 <p>Oliver Schönherr</p>
             </b-col>
         </b-row>
-
-        <!-- Text !-->
-        <h3 class="beschreibung">Das Coding Camp</h3>
-        <h5 class="description">Das Coding Camp, welches durch die Firma Sybit GmbH ins Leben gerufen wurde, hat im Jahr 2020 ein Projekt zu „Sei ein Held rette die Welt“ gestartet. Diese Initiative wurde von Fabienne Schwarz-Loy ins Leben gerufen, um die Welt ein Stück besser zu machen. Um ein Held zu sein, setzt Fabienne sogenannte Impulse ein, welche die Möglichkeit geben sich mit dem Thema „Umweltschutz“ und weiteren wichtigen Gesellschaftlichen Aspekten auseinanderzusetzen. Die Umsetzung dieser Idee erfolgte mithilfe 9 Schüler, 12 Azubis sowie einem Betreuer/Ausbilder von Sybit, welche 5 Tage lang im Coding Camp im Milchwerk in Radolfzell daran arbeiteten. Die Schüler hatten die Challenge nach 5 Tagen selbstständiger Entwicklungszeit bei einem remote Go-Live vor weiteren Sybit Mitarbeitern ihre Webapp zu präsentieren.</h5>
-
+      <b-row class="my-3">
+        <b-col>
+          <h3>Das Coding Camp 2020</h3>
+          <h5>Das Coding Camp, welches durch die Firma Sybit GmbH ins Leben gerufen wurde, hat im Jahr 2020 ein Projekt zu „Sei ein Held rette die Welt“ gestartet. Diese Initiative wurde von Fabienne Schwarz-Loy ins Leben gerufen, um die Welt ein Stück besser zu machen. Um ein Held zu sein, setzt Fabienne sogenannte Impulse ein, welche die Möglichkeit geben sich mit dem Thema „Umweltschutz“ und weiteren wichtigen Gesellschaftlichen Aspekten auseinanderzusetzen. Die Umsetzung dieser Idee erfolgte mithilfe 9 Schüler, 12 Azubis sowie einem Betreuer/Ausbilder von Sybit, welche 5 Tage lang im Coding Camp im Milchwerk in Radolfzell daran arbeiteten. Die Schüler hatten die Challenge nach 5 Tagen selbstständiger Entwicklungszeit bei einem remote Go-Live vor weiteren Sybit Mitarbeitern ihre Webapp zu präsentieren.</h5>
+        </b-col>
+      </b-row>
     </div>
 </b-container>
 </div>
 </template>
 
-<style scoped>
-.background {
-    background-color: #FFFFFFFF;
-}
-.beschreibung {
-    padding-top: 30px;
-
-}
-.description {
-    border: 4px solid black;
-    border-radius: 10px;
-    margin-top: 35px;
-    padding: 17px;
-}
+<style scoped lang="scss">
 .names {
     padding-top: 45px;
-}
-.image {
-    transition: box-shadow .3s;
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-}
-.image:hover {
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-}
-.u {
-    padding-top: 30px;
-    letter-spacing: 40px;
-    font-size: 400%;
-    color: gray;
 }
 </style>
