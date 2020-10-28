8a969ed652bf6135f5c9205a87cec14daa283e95
Franke <efk@sybit.de>
2020-10-28 11:47:48 +0100

    refactor

--- src/components/element/CookieBanner.vue
+++ src/components/element/CookieBanner.vue
@@ -5,7 +5,7 @@
     message="Um unsere Website zu optimieren und für Sie individueller zu gestalten, setzen wir Cookies für verschiedene Zwecke ein. Einige sind essentiell während andere optional aktiviert und deaktiviert werden können. Umfassende Informationen zu diesen Cookies finden Sie in unserer Datenschutzerklärung. Dort können Sie Ihre ganz individuellen Einstellungen jederzeit über den Button unten links anpassen."
     buttonLinkText="Datenschutzerklärung"
     buttonLink="/privacy"
-    ></cookie-law>
+    >
   </footer>
 </template>
 
