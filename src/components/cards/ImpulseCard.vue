2d0cdd26ce4b3f6011690e032dd7bec5d9f29404
chrisWolter <wolter-christian@outlook.com>
2020-10-28 14:28:27 +0100

    Fix PR Comments

--- src/components/cards/ImpulseCard.vue
+++ src/components/cards/ImpulseCard.vue
@@ -31,13 +31,49 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.card-body{
+  margin-left: auto;
+  margin-right: auto;
+  width: 263px;
+  position:relative;
+  padding: 0;
+  height: 409px;
+}
+
+.card-background{
+  width: 100%;
+}
+
+.card-background img{
+  width: 100%;
+}
+
+.card-content{
+  position: absolute;
+  height: 100%;
+  width: 100%;
+}
+
+.card-text{
+  height: 50%;
+  text-align: center;
+  padding-top: 25px;
+  padding-bottom: 10px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
+.card-text-space{
+  padding-top: 30px;
+}
+
 .flip-card{
   position: relative;
   width: 100%;
   height: 100%;
   transition: transform 0.8s;
   transform-style: preserve-3d;
-  }
+}
 
 .card-body:hover .flip-card{
   transform: rotateY(180deg);
@@ -53,39 +89,4 @@ export default {
   transform: rotateY(180deg);
 }
 
-.card-background{
-width: 100%;
-}
-
-.card-background img{
-width: 100%;
-}
-
-.card-body{
-margin-left: auto;
-margin-right: auto;
-width: 263px;
-position:relative;
-padding: 0;
-height: 409px;
-}
-
-.card-content{
-position: absolute;
-height: 100%;
-width: 100%;
-}
-
-.card-text{
-height: 50%;
-text-align: center;
-padding-top: 25px;
-padding-bottom: 10px;
-padding-left: 15px;
-padding-right: 15px;
-}
-
-.card-text-space{
-padding-top: 30px;
-}
 </style>
