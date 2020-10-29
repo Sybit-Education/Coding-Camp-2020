184877ab7b456a388b7432c27d60b3b5c6d16298
EliasFrank2004 <efrk2712@gmail.com>
2020-10-29 14:45:15 +0100

    Added second button for toggle

--- src/components/cards/AssignButton.vue
+++ src/components/cards/AssignButton.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
-        <b-button pill class="bg-info d-flex"><vue-fontawesome icon="plus-circle" color="white" size="2"></vue-fontawesome></b-button>
+        <b-button pill id="button" class="bg-info d-flex"><vue-fontawesome icon="plus-circle" color="white" size="2"></vue-fontawesome></b-button>
+        <b-button pill id="button" class="bg-success d-flex"><vue-fontawesome icon="check-circle" color="white" size="2"></vue-fontawesome></b-button>
     </div>
 </template>
