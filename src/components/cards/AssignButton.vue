9971b0a085f2096ed1dcb843451bafd6adb2c3a8
mirko-pleli <Mirko.Pleli@sybit.de>
2020-10-29 13:47:45 +0100

    Add methods to save assigned card.

--- src/components/cards/AssignButton.vue
+++ src/components/cards/AssignButton.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <b-button pill><vue-fontawesome icon="plus-circle" color="white" size="2"></vue-fontawesome></b-button>
+        <b-button pill id="button" class="bg-info d-flex"><vue-fontawesome icon="plus-circle" color="white" size="2"></vue-fontawesome></b-button>
+        <b-button pill id="button2" class="bg-success d-flex"><vue-fontawesome icon="check-circle" color="white" size="2"></vue-fontawesome></b-button>
     </div>
 </template>
 
