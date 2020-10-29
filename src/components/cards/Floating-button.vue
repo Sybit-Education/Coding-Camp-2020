5092825b941a67b4ee86886359f281d9b5319086
EliasFrank2004 <efrk2712@gmail.com>
2020-10-29 12:08:16 +0100

    Add floating Bbutton file

--- src/components/cards/Floating-button.vue
+++ src/components/cards/Floating-button.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>hello </h1>
+    <b-row id="button" class="d-flex flex-column align-items-center">
+       <b-col><b-button pill key="item.id" variant="info"  size="lg">+</b-button></b-col>
+    </b-row>
 </template>
+
+<style>
+#button {
+    position: relative;
+    margin-top: -70px;
+    margin-left: 170px;
+}
+</style>
