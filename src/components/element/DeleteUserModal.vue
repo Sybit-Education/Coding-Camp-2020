c4d826fb7d6916db0767023a702e6d253187f037
Degener <jde@sybit.de>
2020-10-29 10:24:50 +0100

    delete User funcion

--- src/components/element/DeleteUserModal.vue
+++ src/components/element/DeleteUserModal.vue
@@ -2,7 +2,7 @@
   <div>
   <b-button  v-b-modal.modal-1 variant="outline-primary">Account Löschen</b-button>
 
-<b-modal id="modal-1" title="Account Löschen">
+<b-modal id="modal-1" title="Account Löschen" hide-footer>
     <b-row>
         <b-col>
             <p>Bestätigen Sie mit der eingabe ihres Passwortes das Löschen ihres Accounts.</p>
@@ -13,6 +13,23 @@
             <b-form-input type="password" placeholder="Passwort" required v-model="password"></b-form-input>
         </b-col>
     </b-row>
+     <b-row class="py-3 modal-grey mb-n3">
+        <b-col cols="12" class="d-flex justify-content-end">
+          <b-button
+            @click="deleteAccount()"
+            class="d-inline"
+            variant="danger"
+          >Löschen
+          </b-button>
+          <b-button
+            @click="hideModal()"
+            class="d-inline ml-2"
+            variant="secondary"
+          >
+            Abbrechen
+          </b-button>
+        </b-col>
+      </b-row>
   </b-modal>
 
   </div>
@@ -27,7 +44,6 @@ export default {
   },
   methods: {
     deleteAccount () {
-      console.log('geh rein')
       this.$store.dispatch('Auth/deleteUser', this.password)
     },
     hideModal () {
@@ -35,3 +51,4 @@ export default {
     }
   }
 }
+</script>
