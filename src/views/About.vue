f08732f3b9a03cd0f0476842f490fe66c99e0db9
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-10-30 13:45:22 +0100

    add logo

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,13 +7,16 @@
   </b-row>
   <b-row class="my-3">
     <b-col>
-      <p>Die App</p>
+      <b-img :src="logo" fluid-grow alt="Logo" />
+
+      <h3>Die App</h3>
       <p>Fabienne Schwarz-Loy hat durch „Sei ein Held rette die Welt“ eine Initiative gestartet mit der spielerisch leicht, jeder etwas für die Umwelt tun kann.</p>
       <p>Wir liefern dir einmal pro Woche einen kurzen Impuls dazu, wie du in wenigen Sekunden und mit wenigen Gedanken oder Handgriffen die Welt rettest.</p>
       <p>Der Hintergrund: Unsere Welt ist unendlich schön und unendlich wertvoll. Das sollte so bleiben, aber leider steckt sie aktuell in ziemlich großen Schwierigkeiten. Daher ist jeder Mensch, der zur Rettung beiträgt, ein Held und wir finden, das sollte belohnt werden.</p>
       <p>Wir bieten dir Impulse zu folgenden Themen an: <br>      Umwelt- und Klimaschutz, Gerechtigkeit, Toleranz und wie alles miteinander zusammenhängt.</p>
       <p>Das Beste daran ist, dass es nicht schwierig ist und keinem weh tut. Du sparst Geld, Zeit, Nerven und/oder gewinnst Gesundheit, wertvolle Erfahrungen, Einblicke und Gewohnheiten. Wie du das machst wird dir auf der Impulskarte genauer beschrieben.</p>
-      <h1>Spielregeln</h1>
+
+      <h3>Spielregeln</h3>
       <p>Die Spielregeln für "Sei ein Held rette die Welt", sind:</p>
       <ul>
         <li>Alltags Challenges bekommen weniger Punkte</li>
@@ -22,49 +25,62 @@
         <li>Nach Erledigung des Impulses kanst du dieses Bestätigen und erhältst dafür Punkte</li>
         <li>Du kannst nur alle 24 Stunden deine Impulse bestätigen </li>
       </ul>
-      <h1>Hilfestellung</h1>
+
+      <h3>Hilfestellung</h3>
       <p>Die Hilfestellungen für "Sei ein Held rette die Welt", sind:</p>
 
       <ol>
         <li>
-            <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-users" style="color: black; font-size: 1rem;"></i>
-          Optionen</li>
-        <ul>
-          <li>Man wird weitergeleitet auf: Über uns, Datenschutz, Impressum, das Team, Administration</li>
+            <i aria-hidden="true" class="fa fa-users" style="color: black; font-size: 1rem;"></i>
+          Optionen
 
-        </ul>
-            <li>
-              <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-heart" style="color: black; font-size: 1.25rem;"></i>
-            Favoriten</li>
-        <ul>
-          <li>Diene Impulskarten werden dir hier angezeit</li>
-        </ul>
+          <ul>
+            <li>Man wird weitergeleitet auf: Über uns, Datenschutz, Impressum, das Team</li>
 
+          </ul>
+        </li>
         <li>
-          <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-home" style="color: black; font-size: 1.5rem;"></i>
-          Home Botton / Startseite</li>
-        <ul>
-          <li>Hier findest du neue Impulse</li>
-        </ul>
-
+            <i aria-hidden="true" class="fa fa-heart" style="color: black; font-size: 1.25rem;"></i>
+            Favoriten
+            <ul>
+              <li>Deine Impulskarten werden dir hier angezeit</li>
+            </ul>
+        </li>
         <li>
-          <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-list-ol" style="color: black; font-size: 1.25rem;"></i>
-          Rangliste</li>
-        <ul>
-          <li>Man kann seine Punkte mit Freunden und anderen Nutzer vergleichen</li>
-        </ul>
-
+          <i aria-hidden="true" class="fa fa-home" style="color: black; font-size: 1.5rem;"></i>
+          Home Botton / Startseite
+          <ul>
+            <li>Hier findest du neue Impulse</li>
+          </ul>
+        </li>
         <li>
-          <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-user" style="color: black; font-size: 1.25rem;"></i>
-          Profil</li>
-        <ul>
-          <li>Profilbild</li>
-          <li>Benutzername, E-Mail</li>
-          <li>Abmeldung</li>
-          <li>Account löschen</li>
-        </ul>
+          <i aria-hidden="true" class="fa fa-list-ol" style="color: black; font-size: 1.25rem;"></i>
+          Rangliste
+          <ul>
+            <li>Man kann seine Punkte mit Freunden und anderen Nutzer vergleichen</li>
+          </ul>
+        </li>
+        <li>
+          <i aria-hidden="true" class="fa fa-user" style="color: black; font-size: 1.25rem;"></i>
+          Profil
+          <ul>
+            <li>Profilbild</li>
+            <li>Benutzername, E-Mail</li>
+            <li>Abmeldung</li>
+            <li>Account löschen</li>
+          </ul>
+        </li>
       </ol>
     </b-col>
   </b-row>
 </b-container>
 </template>
+<script>
+export default {
+  computed: {
+    logo () {
+      return require('@/assets/Logo-sei-ein-held.png')
+    }
+  }
+}
+</script>
