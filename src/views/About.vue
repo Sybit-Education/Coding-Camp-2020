67bc45d2b51635639ea3fddc9324e76b9401f598
robingundlach <69695642+robingundlach@users.noreply.github.com>
2020-10-30 10:29:13 +0100

    Add Hilfestellung List, Icon and text

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,6 +10,52 @@
       Die App ...
       <h1>Spielregeln</h1>
       <p>Die Spielregeln für "Sei ein Held rette die Welt", sind:</p>
+      <ul>
+        <li>Alltags Challenges bekommen weniger Punkte</li>
+        <li>Aufwendigere Impulse bekommen dagegen eine höhere Punktwertung</li>
+        <li>Du verlierst deine Punkte, wenn du nicht aktiv genug teilnimmst</li>
+        <li>Nach Erledigung des Impulses kanst du dieses Bestätigen und erhältst dafür Punkte</li>
+        <li>Du kannst nur alle 24 Stunden deine Impulse bestätigen </li>
+      </ul>
+      <h1>Hilfestellung</h1>
+      <p>Die Hilfestellung für "Sei ein Held rette die Welt", sind:</p>
+
+      <ol>
+        <li>
+            <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-users" style="color: black; font-size: 1rem;"></i>
+          Optionen</li>
+        <ul>
+          <li>Man wird weitergeleitet auf: Über uns, Datenschutz, Impressum, das Team, Administration</li>
+
+        </ul>
+            <li>
+              <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-heart" style="color: black; font-size: 1.25rem;"></i>
+            Favoriten</li>
+        <ul>
+          <li></li>
+        </ul>
+
+        <li>
+          <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-home" style="color: black; font-size: 1.5rem;"></i>
+          Home Botton / Startseite</li>
+        <ul>
+          <li>Hier findest du neue Impulse</li>
+        </ul>
+
+        <li>
+          <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-list-ol" style="color: black; font-size: 1.25rem;"></i>
+          Rangliste</li>
+        <ul>
+          <li>Man kann seine Punkte mit Freunden und anderen Nutzer vergleichen</li>
+        </ul>
+
+        <li>
+          <i data-v-1123d0ad="" data-v-a17bb30c="" aria-hidden="true" class="fa fa-user" style="color: black; font-size: 1.25rem;"></i>
+          Profil</li>
+        <ul>
+          <li>Man kann seine Punkte mit Freunden und anderen Nutzer vergleichen</li>
+        </ul>
+      </ol>
     </b-col>
   </b-row>
 </b-container>
