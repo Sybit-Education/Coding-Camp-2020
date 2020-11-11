d5541421f02a89a2bc6318934576720402594a21
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-11-11 11:26:36 +0100

    add logo to header

--- src/components/_base/Header.vue
+++ src/components/_base/Header.vue
@@ -1,8 +1,12 @@
 <template>
-  <div id="profile-dropdown">
-
-    <b-navbar id="navigationBar" class="container-fluid pt-0 pl-0">
-      <b-navbar-brand><h2>{{ headerTitle }}</h2></b-navbar-brand>
+  <b-navbar id="navigationBar">
+    <b-container>
+      <b-navbar-brand>
+        <h1>
+          <b-img :src="logo" class="logo" alt="Sei ein Held" />
+          {{ headerTitle }}
+        </h1>
+      </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -10,8 +14,7 @@
         <b-navbar-nav class="ml-auto">
           <b-nav-item-dropdown no-caret right>
             <template v-slot:button-content>
-              <vue-fontawesome icon="ellipsis-v" color="black" size="2"></vue-fontawesome>
-
+              <vue-fontawesome icon="ellipsis-v" color="black" ></vue-fontawesome>
             </template>
             <b-dropdown-item v-b-modal.modal-delete-account variant="danger" to="/profile">
                <DeleteUserModal></DeleteUserModal>
@@ -19,9 +22,8 @@
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
-    </b-navbar>
-
-  </div>
+    </b-container>
+  </b-navbar>
 </template>
 
 <script>
@@ -38,22 +40,24 @@ export default {
   computed: {
     isProfile () {
       return this.$router.history.current.path === '/profile'
+    },
+    logo () {
+      return require('@/assets/logo.png')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-#profile-dropdown {
-  width: 100%;
-  height: 75px;
+h1 {
+  font-size: 1.5rem;
+}
+#navigationBar {
   background-color: $color-yellow;
-}
 
-h2 {
-  font-weight: bold;
-  padding-left: 16px;
-  line-height: 75px;
+  .logo {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
 }
-
 </style>
