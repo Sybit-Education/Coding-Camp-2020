e3d1787065e2163b5c90cf56065fc9ced512be99
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-12-02 00:32:09 +0100

    justify Buttons

--- src/components/navigation/BottomNavigation.vue
+++ src/components/navigation/BottomNavigation.vue
@@ -1,34 +1,27 @@
 <template>
   <b-navbar class="navbar" type="dark" variant="dark" fixed="bottom">
     <b-container>
-      <b-navbar-nav>
-        <b-nav-item :to="{ name: 'AssignedImpulse'}">
+      <b-navbar-nav justified align="start">
+        <b-nav-item :to="{ name: 'AssignedImpulse'}" class="mr-3">
           <vue-fontawesome icon="heart" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
 
-        <b-nav-item :to="{ name: 'Home'}">
+        <b-nav-item :to="{ name: 'Home'}" class="mx-3">
           <vue-fontawesome icon="home" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
 
-        <b-nav-item :to="{ name: 'Highscore'}">
+        <b-nav-item :to="{ name: 'Highscore'}" class="mx-3">
           <vue-fontawesome icon="list-ol" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
 
-        <b-nav-item :to="{ name: 'Profile'}">
+        <b-nav-item :to="{ name: 'Profile'}" class="mx-3">
           <vue-fontawesome icon="user" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
-      </b-navbar-nav>
 
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item :to="{ name: 'Options'}" right>
+        <b-nav-item :to="{ name: 'Options'}"  class="mx-3">
           <vue-fontawesome icon="ellipsis-v" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
       </b-navbar-nav>
     </b-container>
   </b-navbar>
 </template>
-
-<script>
-export default {
-}
-</script>
