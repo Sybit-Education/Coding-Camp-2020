9c78eecb908e17dd98854483896e758707ee3c78
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-12-29 18:12:08 +0100

    Add specific vuex store for PublishingState

---
+++ src/components/admin/_base/PublishingStateLabel.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-badge
+    class="publishing-state"
+    v-if="publishingState"
+    v-bind:style="color"
+    v-text="publishingState.name"
+  />
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'PublishingStateLabel',
+  props: {
+    publishingStateId: {
+      type: String,
+      required: false
+    }
+  },
+  data () {
+    return {
+      publishingState: null
+    }
+  },
+  mounted () {
+    this.publishingState = this.getSelected(this.publishingStateId)
+  },
+  computed: {
+    color () {
+      return this.publishingState ? `background-color: ${this.publishingState.color}` : ''
+    },
+    ...mapGetters({
+      getSelected: 'PublishingState/getSelected'
+    })
+  }
+}
+</script>
