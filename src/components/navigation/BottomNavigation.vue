e3e66a38e881b7812b11cbc7a428711fd3829964
Stephan Strittmatter <stritti@users.noreply.github.com>
2021-01-07 10:23:14 +0100

    convert named navigations to urls

--- src/components/navigation/BottomNavigation.vue
+++ src/components/navigation/BottomNavigation.vue
@@ -2,30 +2,26 @@
   <b-navbar  type="dark" variant="dark" fixed="bottom">
     <b-container>
       <b-navbar-nav justified align="start">
-        <b-nav-item :to="{ name: 'AssignedImpulse'}" class="mr-2 mr-md-4">
+        <b-nav-item to="/assigned" class="mr-2 mr-md-4">
           <vue-fontawesome icon="heart" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
 
-        <b-nav-item :to="{ name: 'Home'}" class="mx-2 mx-md-4">
+        <b-nav-item to="/" class="mx-2 mx-md-4">
           <vue-fontawesome icon="home" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
 
-        <b-nav-item :to="{ name: 'Highscore'}" class="mx-2 mx-md-4">
+        <b-nav-item to="/highscore" class="mx-2 mx-md-4">
           <vue-fontawesome icon="list-ol" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
 
-        <b-nav-item :to="{ name: 'Profile'}" class="mx-2 mx-md-4">
+        <b-nav-item to="/profile" class="mx-2 mx-md-4">
           <vue-fontawesome icon="user" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
 
-        <b-nav-item :to="{ name: 'Options'}"  class="mx-2 mx-md-4">
+        <b-nav-item to="/options"  class="mx-2 mx-md-4">
           <vue-fontawesome icon="ellipsis-v" size="1.75" color="white"></vue-fontawesome>
         </b-nav-item>
       </b-navbar-nav>
     </b-container>
   </b-navbar>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
