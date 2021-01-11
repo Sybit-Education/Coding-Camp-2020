8639ed14fc8c9f77719c27dc7088c549aa880e19
Stephan Strittmatter <stritti@users.noreply.github.com>
2021-01-11 13:21:41 +0100

    also support browser history back

--- src/router/index.js
+++ src/router/index.js
@@ -188,7 +188,22 @@ const router = new VueRouter({
   }
 })
 
-// Getting a router.beforeEach functino which handles the case if the user is logged in or not
+router.beforeResolve((to, from, next) => {
+  if (from.name === 'CardDetail' && to.hash === '') {
+    console.log('from: ', from.params.id)
+    router.replace({ path: to.path, hash: from.params.id })
+  } else {
+    next()
+  }
+})
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+    NProgress.start()
+  }
+  next()
+})
+
 router.beforeEach((to, from, next) => {
   const currentUser = $auth.currentUser
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
@@ -201,13 +216,6 @@ router.beforeEach((to, from, next) => {
   } else {
     next()
   }
-})
-
-router.beforeResolve((to, from, next) => {
-  if (to.name) {
-    NProgress.start()
-  }
-  next()
 })
 
 router.afterEach((to, from) => {
