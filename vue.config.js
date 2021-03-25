37a258254b368bf10891ce5b33c5f60662f5178a
Stephan Strittmatter <stritti@users.noreply.github.com>
2021-03-25 14:43:14 +0100

    update PWA config

--- vue.config.js
+++ vue.config.js
@@ -33,6 +33,73 @@ module.exports = {
     }
   },
   pwa: {
+    name: 'Sei ein Held',
+    themeColor: '#FBFF00',
+    msTileColor: '#FBFF00',
+
+    manifestOptions: {
+      name: 'Sei ein Held - rette die Welt',
+      short_name: 'Sei ein Held',
+      background_color: '#FBFF00',
+      icons: [
+        {
+          src: './img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+
+        {
+          src: './img/icons/apple-touch-icon-60x60.png',
+          sizes: '60x60',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/apple-touch-icon-76x76.png',
+          sizes: '76x76',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/apple-touch-icon-120x120.png',
+          sizes: '120x120',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/apple-touch-icon-152x152.png',
+          sizes: '152x152',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/apple-touch-icon-180x180.png',
+          sizes: '180x180',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/favicon-16x16.png',
+          sizes: '16x16',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/mstile-144x144.png',
+          sizes: '144x144',
+          type: 'image/png'
+        }
+      ]
+    },
     workboxOptions: {
       // https://github.com/yyx990803/register-service-worker/issues/14
       // https://stackoverflow.com/questions/54145735/vue-pwa-not-getting-new-content-after-refresh
